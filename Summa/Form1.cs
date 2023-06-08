da389dacf63d8fb552a37c6410e25a1db4f6d03a
AdamAkhooo <adamjamal0505@gmail.com>
2023-06-08 15:32:01 +0200

    Add project files.

---
+++ Summa/Form1.cs
@@ -0,0 +1,45 @@
+namespace Summa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1, n2, sum;
+            double.TryParse(textBox1.Text, out n1);
+            double.TryParse(textBox2.Text, out n2);
+            sum = n1 + n2;
+            label3.Text = sum.ToString();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
